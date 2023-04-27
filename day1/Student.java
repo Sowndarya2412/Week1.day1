baee186d9498bd2f197a46a582354b05a0e7c460
Sowndarya <131742587+Sowndarya2412@users.noreply.github.com>
2023-04-27 07:57:54 +0530

    Add files via upload
    Week1.day1

---
+++ day1/Student.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class Student {
+
+	String studentName = "Sowndarya";
+	int rollNo = 52;
+	String collegeName = "Krishna Arts and Science college";
+	float markScored = 91.36f;
+	double cgpa = 9.1;
+
+}
